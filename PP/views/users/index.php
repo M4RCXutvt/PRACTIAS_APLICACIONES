6fcb975b58c69d3fac95cfd8228ca583379ce780
M4RCXutvt <al222210685@gmail.com>
2023-08-03 08:57:16 -0600

    practica 2

--- PP/views/users/index.php
+++ PP/views/users/index.php
@@ -10,7 +10,7 @@ include __DIR__ . '/../../layout/header.php';
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">VISTAS USUARIOS</h1>
         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                class="fas fa-plus fa-sm text-white-50"></i> Agregar Usuario</a>
+                class="fas fa-plus fa-sm text-white-50"></i> Agregar Usuarios</a>
     </div>
 
 
