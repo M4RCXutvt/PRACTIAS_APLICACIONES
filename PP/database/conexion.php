a16b6964e01c14b0546b6d1eabb396a4c6a9446c
M4RCXutvt <al222210685@gmail.com>
2023-08-02 23:18:37 -0600

    comprobar conexion

--- PP/database/conexion.php
+++ PP/database/conexion.php
@@ -1,11 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    ESTRUCRA DE MI PROYECTOS
-</body>
-</html>
+<?php
+$server='localhost';
+$user='root';
+$pass='';
+$bd='pv';
+
+
+$conexion=new mysqli($server,$user,$pass,$bd);
+
+if($conexion->connect_errno){
+    die("fallo");
+}else{
+    echo("Se conecto");
+}
+
+
+?>
