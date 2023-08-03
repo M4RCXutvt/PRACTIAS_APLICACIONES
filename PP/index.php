56545f02ade825bb9c449c67e93634a9e8ef05f8
M4RCXutvt <al222210685@gmail.com>
2023-08-02 23:31:12 -0600

    practica 1

--- PP/index.php
+++ PP/index.php
@@ -320,7 +320,7 @@ include("layout/header.php");
                     <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>
                 </div>
                 <div class="card-body">
-                    <p>SB Admin 2 make extensive use of Bootstrap 4 utility classes in order to reduce
+                    <p>SB Admin 2 makess extensive use of Bootstrap 4 utility classes in order to reduce
                         CSS bloat and poor page performance. Custom CSS classes are used to create
                         custom components and custom utility classes.</p>
                     <p class="mb-0">Before working with this theme, you should become familiar with the
